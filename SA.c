603a4cb9f8d6129afe336cf8b2ec8e77ced7a760
Inko23 <29inko29@gmail.com>
2024-10-07 17:13:08 +0900

    third commit by school

--- SA.c
+++ SA.c
@@ -1,15 +1,26 @@
+#include "decs.h"
 
-P = InitialPlacement();
-T = InitialTemperature();
+void SA(){
 
-while(ExitCriterion() == False){
-    while(InnerLoopCriterion() == False){ /*One templerature*/
-        P_new = PerturbPlacementViaMove(P);
-        diff_Cost = Cost(P_new) - Cost(P);
-        r = random(0,1);
-        if(r < pow(e, -diff_Cost/T));{
-            P = P_new; /*Accept move*/
-        }
-    } /*End one temperature*/
-    T = UpdateTemp(T);
+    P = InitialPlacement();
+    T = InitialTemperature();
+    
+    while(ExitCriterion() == False){
+        while(InnerLoopCriterion() == False){ /*One templerature*/
+            P_new = PerturbPlacementViaMove(P);
+            diff_Cost = Cost(P_new) - Cost(P);
+           r = random(0,1);
+            if(r < pow(e, -diff_Cost/T));{
+                P = P_new; /*Accept move*/
+            }
+        } /*End one temperature*/
+        T = UpdateTemp(T);
+    }
 }
+
+
+//InitialPlacement()
+//InitialTemperature()
+//PerturbPlacementViaMove()
+//Cost()
+//UpdateTemp()
